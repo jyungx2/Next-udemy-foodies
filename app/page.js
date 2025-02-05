b65ad42bdb3468ffbdace204680c3044ebb50379
jiyoungnim <cloundyon31@gmail.com>
2025-02-05 17:01:25 +0900

    📝104. Preparing an Image Slideshow

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
@@ -17,6 +20,7 @@ export default function Home() {
           </div>
         </div>
       </header>
+
       <main>
         <section className={classes.section}>
           <h2>How it works</h2>
