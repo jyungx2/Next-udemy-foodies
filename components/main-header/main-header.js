99e5a997047fbc9ce454bec8c34ef8a04a833ad1
jiyoungnim <cloundyon31@gmail.com>
2025-02-05 18:14:01 +0900

    📝 106. Using Client Components Efficiently

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,11 +1,15 @@
+// "use client"; // client component는 가장 작은 범위로 가장 적은 컴포넌트에 한해 설정하는게 이득!
+// 서버 측에 렌더링되면서 얻는 이점들을 사용하기 위해 최대한 React Server Component로 유지하자!
+// 따라서 navigation 바를 만들 때 사용되는 usePathnam()을 위해 메인헤더를 Client Component로 만들지 말고, <Link>를 출력하는 컴포넌트를 따로 만들어서 분리시키는 방향으로 가자!!
+
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
 
 export default function MainHeader() {
-  console.log("Executing");
   return (
     <>
       <MainHeaderBackground />
@@ -18,10 +22,11 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
+
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Browse Meals</NavLink>
             </li>
           </ul>
         </nav>
