b3c2f289459b654817f0686f8a1e41cfd316e70b
jiyoungnim <cloundyon31@gmail.com>
2025-02-06 18:05:39 +0900

    📝 127. Working with Server Action Responses & useFormState(useActionState is only available in React 19 so do not use it!)

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
-// "use client"; // 🚨서버액션을 실행하는 shareMeal 함수를 lib 폴더 안으로 분리하면 에러가 발생하지 않음.
+"use client"; // 🚨서버액션을 실행하는 shareMeal 함수를 lib 폴더 안으로 분리하면 에러가 발생하지 않음.
 // 이유: Next.js의 빌드 프로세스는 서버와 클라이언트 코드를 완전히 분리하여 실행하지 못하기 때문에, 같은 컴포넌트 내에서 "use client"와 "use server"를 함께 사용할 수 없음.
+
+import { useFormState } from "react-dom"; // useActionState: react19 버전에서만 사용가능 & next@14.0.3은 react@18.2.0을 요구하여 에러가 발생하므로 일단은 18ver.에서 제공하는 useFormState 사용.
 
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
@@ -7,8 +9,15 @@ import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
+  // 124. Managing the Form Submission Status with useFormStatus
   // const status = useFormState();
   // // use client 코드 필요 -> 별도 컴포넌트화(MealsFormSubmit)
+
+  // 127. Working with Server Action Responses & useFormState
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+  // ✨ useFormState
+  // 1st param: actual Server action that should be triggered when the form is submitted.
+  // 2st param: initial state(value) before this action is triggered.
 
   return (
     <>
@@ -19,7 +28,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -48,6 +57,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Upload Image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
