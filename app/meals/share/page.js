77a83169d67c229faa5451d1f7ac90c99a788498
jiyoungnim <cloundyon31@gmail.com>
2025-02-06 13:50:36 +0900

    📝 119/120. Introducing & Using Server Actions for Handling Form Submissions

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,22 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    ("use server"); // ⭐️ ShareMealPage 컴포넌트 자체가 이미 서버에서 실행되는 서버 컴포넌트이지만, 그 안에 위치한 함수의 경우, use server라는 코드를 통해 서버에서 실행되는 함수라는 것을 따로 명시해줘야 한다. & async도 붙여줘야 비로소 진정한 server action을 행하는 함수로 만들 수 있음! (You must explicitly state that it belongs to the server by adding this directive inside of them if you wanna create such a Server Action! So to make a function that's guaranteed to execute on the server.)
+
+    const meal = {
+      title: formData.get("title"), // input요소의 name 속성값으로 가져옴
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+    // 📝 120. You can use 'server actions' feature to create such this function that will be triggered when a form is submitted.
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +27,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -16,6 +16,7 @@ export default function ImagePicker({ label, name }) {
     const file = e.target.files[0];
 
     if (!file) {
+      setPickedImage(null);
       return;
     }
 
@@ -51,6 +52,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
